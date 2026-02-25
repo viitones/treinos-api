3d36a0ecfad82cb864c5307b4fb6d39cfe429168
viitones <vitonesmartins@gmail.com>
2026-02-25 15:31:48 -0300

    :sparkles: feat: adicionado fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-const name = ""
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT ?? 3000) });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
