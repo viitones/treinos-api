78305cf6f1c7e0528c49dd1ae04376fd7060272f
viitones <vitonesmartins@gmail.com>
2026-02-25 16:03:50 -0300

    :wrench: chore: geração do fastify swagger

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,61 @@
 import "dotenv/config";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
-import type { ZodTypeProvider } from 'fastify-type-provider-zod';
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { z } from 'zod/v4';
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { z } from "zod/v4";
 
 const app = Fastify({
   logger: true,
 });
 
 app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler)
+app.setSerializerCompiler(serializerCompiler);
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Treinos API",
+      description: "API para o app Treinos",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:3001",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
-  method: 'GET',
-  url: '/',
+  method: "GET",
+  url: "/",
   schema: {
     description: "Hello world endpoint",
-    tags: ['example'],
+    tags: ["Hello world"],
     response: {
       200: z.object({
         message: z.string(),
-      })
-    }
+      }),
+    },
   },
   handler: () => {
     return {
-      message: "Hello world!"
-    }
-  }
+      message: "Hello world!",
+    };
+  },
 });
 
 try {
